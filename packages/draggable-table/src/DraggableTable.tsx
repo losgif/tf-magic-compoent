027061150685cb2643871ecc6a172743892b70ca
郑余靖 <24907@etransfar.com>
2021-12-02 18:59:58 +0800

    feat: 更新表格组件

---
+++ packages/draggable-table/src/DraggableTable.tsx
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+// @ts-ignore
+import ReactDragListView from 'react-drag-listview';
+import Table from './Table'
+import { TableProps } from 'antd/lib/table/interface'
+import { cloneDeep } from 'lodash'
+import { ColumnProps } from 'antd/lib/table'
+
+/**
+ * 可拖拽表格组件Props
+ * 注意，Columns参数中，key 必须唯一且必传
+ */
+type DraggableTableProps<T> = TableProps<T> & {
+  storageKey: string,
+}
+
+/**
+ * 可拖拽表格组件
+ *
+ * @param storageKey 持久化标识Key
+ * @param props
+ * @constructor
+ */
+const DraggableTable = <T extends unknown> ({ storageKey, ...props }: DraggableTableProps<T>) => {
+  const [columns, setColumns] = useState<ColumnProps<T>[]>()
+
+  /**
+   * 获取持久化保存表格排序数据
+   */
+  const getColumnKeys = (): (keyof T)[] => {
+    return JSON.parse(localStorage.getItem(storageKey))
+  }
+
+  /**
+   * 持久化保存表头排序数据
+   *
+   * @param columnKeys
+   */
+  const setColumnKeys = (columnKeys: (keyof T)[]) => {
+    localStorage.setItem(storageKey, JSON.stringify(columnKeys))
+  }
+
+  /**
+   * 响应拖拽事件
+   *
+   * @param fromIndex
+   * @param toIndex
+   */
+  const handleDragColumnsEnd = (fromIndex: number, toIndex: number) => {
+    const cloneColumns = cloneDeep(columns)
+    const item = cloneColumns.splice(fromIndex - +!!props.rowSelection, 1)[0];
+
+    cloneColumns.splice(toIndex - +!!props.rowSelection, 0, item);
+
+    const columnKeys: (keyof T)[] = []
+    cloneColumns.forEach((column) => {
+      if (!column.key) {
+        throw new Error('属性 key 必须指定值')
+      }
+
+      if (columnKeys.includes(column.key as keyof T)) {
+        throw new Error('属性 key 必须唯一')
+      }
+
+      columnKeys.push(column.key as keyof T)
+    })
+
+    setColumnKeys(columnKeys)
+    setColumns(cloneColumns);
+  }
+
+  /**
+   * 初始化表头
+   */
+  useEffect(() => {
+    // 获取表头排序数据
+    const columnKeys = getColumnKeys()
+
+    if (Array.isArray(columnKeys) && columnKeys.length === props.columns.length) {
+      const cloneColumns = columnKeys.map(columnKey => {
+        for (let i = 0; i < props.columns.length; i++) {
+          if (props.columns[i].key === columnKey) {
+            return props.columns[i]
+          }
+        }
+      })
+
+      setColumns(cloneColumns)
+    } else {
+      setColumnKeys([])
+      setColumns(props.columns)
+    }
+  }, [])
+
+  return (
+    <ReactDragListView.DragColumn
+      onDragEnd={ handleDragColumnsEnd }
+      nodeSelector='th'
+    >
+      <Table { ...props } columns={ columns }/>
+    </ReactDragListView.DragColumn>
+  );
+};
+
+export default DraggableTable;
---
+++ packages/draggable-table/src/Table/index.tsx
@@ -0,0 +1,27 @@
+/*
+ * 统一修改 Ant-design 的样式
+ * @Author: wenzhe
+ * @Date: 2019-07-01 11:25:50
+ * @Last Modified by: wenzhe
+ * @Last Modified time: 2019-07-01 14:49:13
+ */
+import React from 'react';
+import { Table } from 'antd';
+import classNames from 'classnames';
+import styles from './index.less';
+import { TableProps } from 'antd/es/table'
+
+const styledTable = <T extends unknown>(props: TableProps<T>) => {
+  const { className, ...restProps } = props;
+  const prevClassName = `${styles.commonTable}`;
+
+  return (
+    <Table
+      className={classNames(prevClassName, className)}
+      {...restProps}
+    />
+  );
+};
+
+export default styledTable;
+
