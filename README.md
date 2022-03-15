e3fc7745d121a8386f8865b3b2ee6d22601ac54b
losgif <losgif@gmail.com>
2022-03-15 15:17:04 +0800

    feat: 去lerna化

---
+++ README.md
@@ -0,0 +1,6 @@
+---
+title: 首页
+---
+
+# TF-Magic-Component
+本仓库提供一些常用的公共封装组件
--- typed-css.d.ts
+++ typed-css.d.ts
@@ -1,11 +1,11 @@
 // less模块声明
 declare module '*.less' {
-  const content: { [key: string]: any }
-  export default content
+  const less: { [key: string]: any }
+  export default less
 }
 
 // css模块声明
 declare module '*.css' {
-  const content: { [key: string]: any }
-  export default content
+  const css: { [key: string]: any }
+  export default css
 }
