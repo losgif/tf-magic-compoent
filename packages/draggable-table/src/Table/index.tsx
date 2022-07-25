82a3b841c052e1653b79fa6de2a1153d7e25a78e
losgif <losgif@gmail.com>
2022-07-25 15:24:18 +0800

    feat(draggable-table): 支持设置全局保存columnKeys方法

--- packages/draggable-table/src/Table/index.tsx
+++ packages/draggable-table/src/Table/index.tsx
@@ -1,17 +1,10 @@
-/*
- * 统一修改 Ant-design 的样式
- * @Author: wenzhe
- * @Date: 2019-07-01 11:25:50
- * @Last Modified by: wenzhe
- * @Last Modified time: 2019-07-01 14:49:13
- */
 import React from 'react';
 import { Table } from 'antd';
 import classNames from 'classnames';
 import styles from './index.module.less';
 import { TableProps } from 'antd/es/table'
 
-const styledTable = <T extends unknown>(props: TableProps<T>) => {
+const styledTable = <T extends object>(props: TableProps<T>) => {
   const { className, ...restProps } = props;
   const prevClassName = `${styles.commonTable}`;
 
