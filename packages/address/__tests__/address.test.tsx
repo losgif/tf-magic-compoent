0a17c16927fe7b2f7ab2a4b630e9075b164a32ea
losgif <losgif@gmail.com>
2022-08-18 18:55:33 +0800

    fix: type error

--- packages/address/__tests__/address.test.tsx
+++ packages/address/__tests__/address.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import PopupAddress from '..'
+import PopupAddress from '../src'
 
 describe('@tf-magic/address', () => {
   test('popup address component renders correctly', () => {
